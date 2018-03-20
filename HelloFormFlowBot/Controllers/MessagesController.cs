6a68f9c569677e93a885433123ffd3b1fca692a2
Salah Eddine Bachir-Cherif <gs_bachircherif@esi.dz>
2018-03-20 23:20:40 +0100

    Ajoutez des fichiers projet.

---
+++ HelloFormFlowBot/Controllers/MessagesController.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using MySql.Data.MySqlClient;
+
+namespace HelloFormFlowBot
+{
+
+
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        internal static IDialog<ProfileForm> MakeRootDialog()
+        {
+            return Chain.From(() => FormDialog.FromForm(ProfileForm.BuildForm));
+        }
+
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            // Detect if this is a Message activity
+            if (activity.Type == ActivityTypes.Message)
+            {
+                // Get any saved values
+                StateClient sc = activity.GetStateClient();
+                BotData userData = sc.BotState.GetPrivateConversationData(
+                     activity.ChannelId, activity.Conversation.Id, activity.From.Id);
+
+                var boolProfileComplete = userData.GetProperty<bool>("ProfileComplete");
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+
+                if (!boolProfileComplete)
+                {
+                    // Call our FormFlow by calling MakeRootDialog
+                    await Conversation.SendAsync(activity, MakeRootDialog);
+                }
+                else
+                {
+                    var Name = userData.GetProperty<string>("Name");
+                    var Email = userData.GetProperty<string>("Email");
+                    var Gender = userData.GetProperty<string>("Sexe");
+                    var Choice = userData.GetProperty<string>("Choice");
+                    var Studie = userData.GetProperty<string>("Studie");
+                    var Motivation = userData.GetProperty<string>("Motivation");
+                    var WhatWeEarn = userData.GetProperty<string>("WhatWeEarn");
+                    var centreInteret = userData.GetProperty<string>("centreInteret");
+
+                    // Tell the user their profile is complete
+                    System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                    sb.Append("Votre Reponse est enregistré\n\n");
+
+                    // Create final reply
+                    Activity replyMessage = activity.CreateReply(sb.ToString());
+                    var conn = new DBConnect();
+
+                    var insert = "INSERT INTO members VALUES (11, '" + Name + "', '" + Email + "', '" + WhatWeEarn + "', '" + Motivation + "',' " + centreInteret + "',' " + Gender + "',' " + Choice + "',' " + Studie + "', NULL, NULL)";
+                    conn.Insert(insert);
+                    await connector.Conversations.ReplyToActivityAsync(replyMessage);
+                }
+            }
+            else
+            {
+                // This was not a Message activity
+                HandleSystemMessage(activity);
+            }
+
+            // Send response
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+    public class DBConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        //Constructor
+        public DBConnect()
+        {
+            Initialize();
+        }
+
+        //Initialize values
+        private void Initialize()
+        {
+            //server = "212.1.212.234";
+            server = "localhost";
+            //database = "cseclubo_messanger_inscription";
+            database = "s";
+            //uid = "cseclubo_salah";
+            uid = "root";
+            //password = "[ sD=;fILrGuR= ]";
+            password = "";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                return false;
+            }
+
+        }
+
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                return false;
+            }
+        }
+
+        //Insert statement
+        public void Insert(string query)
+        {
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+        public bool Check()
+        {
+            if (this.OpenConnection() == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ HelloFormFlowBot/ProfileForm.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using System.Text.RegularExpressions;
+public enum Ecc { Esi = 1, Autre };
+public enum Gendr { Garçon = 1, Fille };
+
+namespace HelloFormFlowBot
+{
+    [Serializable]
+    public class ProfileForm
+    {
+        
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Gendr Sexe { get; set; }
+        public Ecc Choice { get; set; }
+        public string Studie { get; set; }
+        public string Motivation { get; set; }
+        public string WhatWeEarn { get; set; }
+        public string centreInteret { get; set; }
+        
+
+        // This method 'builds' the form 
+        // This method will be called by code we will place
+        // in the MakeRootDialog method of the MessagesControlller.cs file
+        public static IForm<ProfileForm> BuildForm()
+        {
+            Regex rgx = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            return new FormBuilder<ProfileForm>()
+           .Message("Tu veux rejoindre le CSE ? Pas de problèmes. Mais avant toute chose, j’aimerais bien qu’on fasse connaissance, ça te dit?")
+            .Message("Moi je suis le CSE bot")
+            .Field(nameof(Name), "Et toi tu t’appelles comment?")
+            .Message("Ravi {Name}")
+            .Message("Fille ou un garçon ?")
+            .Field(nameof(Sexe))
+            .Message("Enchanté !")
+            .Field(nameof(centreInteret), "Je voudrais en savoir un peu plus sur toi ! Dis-moi, quels sont tes centres d’intérêts ?")
+            .Message("Passionnant !")
+            .Message("Moi j’aime bien tout ce qui est informatique.")
+            .Message("En parlant d’informatique, je suis de l’ESI, et toi {Name} tu étudies où?")
+            .Field(nameof(Choice))
+            .Message("Ah super :D !")
+            .Field(nameof(Studie), "Et tu es en quelle année? {||}")
+            .Field(nameof(Motivation), "Tu sais, on essaye d’améliorer la vie étudientine à l’ESI, comment penses tu nous aider?")
+            .Message("Je vois Cool! :D")
+            .Field(nameof(WhatWeEarn), "Une dernière question, le plus important, qu’attends tu de nous?")
+            .Message("On va faire de notre mieux pour ne pas te décevoir :D !")
+            .Message("Super!")
+            .Field(nameof(Email), "Hey avant de partir, je voudrais avoir ton e-mail afin qu’on reste en contact!",
+            validate: async (state, value) =>
+            {
+                var result = new ValidateResult { IsValid = true, Value = value };
+                var address = (value as string);
+
+                if (!rgx.IsMatch(address))
+                {
+                    result.Feedback = "Incorrect e-mail";
+                    result.IsValid = false;
+                }
+
+                return result;
+            })
+            .AddRemainingFields()
+            .Message("Tous-est correct?")
+            .Confirm("Si vous voulez changez quelque chose entrez Non ,Sinon entrez Oui")
+            .Message("Merci {Name} à très bientôt pour de nouvelles aventures!")
+            .Message("entrez n'importe quoi pour valider votre réponse")
+            .OnCompletion(async (context, profileForm) =>
+                    {
+                        // Set BotUserData
+                        context.PrivateConversationData.SetValue<bool>(
+                            "ProfileComplete", true);
+                        context.PrivateConversationData.SetValue<string>(
+                            "Name", profileForm.Name);
+                        context.PrivateConversationData.SetValue<string>(
+                            "Email", profileForm.Email);
+                        context.PrivateConversationData.SetValue<string>(
+                            "Sexe", profileForm.Sexe.ToString());
+                        context.PrivateConversationData.SetValue<string>(
+                           "Choice", profileForm.Choice.ToString());
+                        context.PrivateConversationData.SetValue<string>(
+                            "Studie", profileForm.Studie);
+                        context.PrivateConversationData.SetValue<string>(
+                           "Motivation", profileForm.Motivation);
+                        context.PrivateConversationData.SetValue<string>(
+                           "WhatWeEarn", profileForm.WhatWeEarn);
+                        context.PrivateConversationData.SetValue<string>(
+                           "centreInteret", profileForm.centreInteret);
+
+
+
+                        // Tell the user that the form is complete
+                    })
+                    .Build();
+        }
+    }
+
+    // This enum provides the possible valuse for the 
+    // Gender property in the ProfileForm class
+    // Notice we start the options at 1 
+    
+}
